522b35ea1edad0235eba1934d880bce27862ccf6
yusuf-saydullayev <yusufsaydullayev@gmail.com>
2024-08-30 16:13:33 +0500

    fix

--- src/routes/home/home.page.tsx
+++ src/routes/home/home.page.tsx
@@ -4,14 +4,7 @@ import { getTasks } from '@/api/getTasks'
 import { columns } from './components/colomuns'
 import { Container } from "@/components/ui/Container"
 import { TableLoading } from './components/table-loading'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/Dialog"
+
 export const HomePage = () => {
   const { isPending, error, data } = useQuery({ queryKey: ['orders'], queryFn: getTasks })
 
@@ -24,18 +17,6 @@ export const HomePage = () => {
       <Container className="pt-4">
         <DataTable data={data} columns={columns} />
       </Container>
-      {/* <Dialog>
-        <DialogTrigger>Open</DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog> */}
     </section>
   )
 }
